866677bc7c79f76684ed8ab15a73628775a6088e
anjomro <github@anjomro.de>
2020-04-12 16:13:40 +0000

    Rebuilt Project

---
+++ build/precache-manifest.e77f851e8a0bb835f9d84bd3b6ab3b90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9307304a6c929a63b2c30753c0031f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2a7841fba87ae77df5a",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7fed25df7ed8a88adecf",
+    "url": "/static/css/main.2ae3a53c.chunk.css"
+  },
+  {
+    "revision": "e2a7841fba87ae77df5a",
+    "url": "/static/js/2.f4eb9418.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f4eb9418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fed25df7ed8a88adecf",
+    "url": "/static/js/main.cf74fa88.chunk.js"
+  },
+  {
+    "revision": "28c836c6390ca2244059",
+    "url": "/static/js/runtime-main.3ce8a40d.js"
+  },
+  {
+    "revision": "b2e600a7e3f7b1ccde7b09451830a0fa",
+    "url": "/static/media/example-cover.b2e600a7.jpg"
+  }
+]);
