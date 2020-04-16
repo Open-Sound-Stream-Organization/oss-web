b66b488f8fabf24e31a40efa507ed3e81d1b0203
PssbleTrngle <nik@somethingcatchy.net>
2020-04-16 18:16:38 +0200

    Audio logic

---
+++ src/api/Audio.tsx
@@ -0,0 +1,116 @@
+import React, { useState, useEffect, useMemo, createContext, useContext } from "react";
+import { ITrack } from "./Models";
+import Api from "./Api";
+import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
+import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
+
+interface PlayerData {
+    track?: ITrack;
+    play: (t?: ITrack) => void;
+    pause: () => void;
+    position: number;
+    shuffle: boolean;
+    repeat: boolean;
+    setShuffle: (b: boolean) => void;
+    setRepeat: (b: boolean) => void;
+    playing: () => boolean;
+}
+
+const context = createContext<PlayerData>({
+    play: () => { },
+    pause: () => { },
+    position: 0,
+    shuffle: false,
+    repeat: false,
+    setShuffle: () => { },
+    setRepeat: () => { },
+    playing: () => false,
+});
+
+export const Provider = context.Provider;
+
+export function usePlayer(): PlayerData {
+    return useContext(context);
+}
+
+/**
+ * Can be used anywhere to access and modify volume
+ * TODO This is where the actual volume logic will happen
+ */
+export function useVolume() {
+    const [volume, setVolume] = useState(40);
+    const [saveVolumed, saveVolume] = useState(volume);
+
+    const toggleVolume = () => {
+        if (volume > 0) {
+            saveVolume(volume);
+            setVolume(0);
+        } else {
+            setVolume(saveVolumed);
+        }
+    }
+
+    return { volume, setVolume, toggleVolume };
+}
+
+export function useCreateAudio(): PlayerData {
+    const [track, setTrack] = useState<ITrack | undefined>(undefined);
+    const [shuffle, setShuffle] = useState(false);
+    const [repeat, setRepeat] = useState(true);
+    const [position, setPosition] = useState(0);
+
+    const update = () => setPosition(audio.currentTime);
+
+    const audio = useMemo(() => new Audio(), []);
+    
+    useEffect(() => {
+        audio.addEventListener('timeupdate', update);
+        return () => audio.removeEventListener('timeupdate', update);
+    }, [audio])
+
+    const play = async (track?: ITrack) => {
+
+        if (track) await Api.audio(track.audio)
+            .then(src => {
+                setTrack(track);
+
+                audio.src = src;
+                audio.currentTime = 0;
+                audio.play();
+
+            }).catch(e => console.log(e));
+
+        else await audio?.play();
+    }
+
+    const pause = () => {
+        audio?.pause();
+    }
+
+    return {
+        track: track ? {...track, length: audio.duration} : undefined,
+        play, pause,
+        shuffle, repeat, setShuffle, setRepeat,
+        playing: () => !!audio && !audio.paused,
+        position,
+    };
+}
+
+export function TrackButton({ track }: { track: ITrack }) {
+    const { play, pause, playing, track: s } = usePlayer();
+    const selected = track.id === s?.id;
+
+    const onClick = () => {
+        if (playing()) pause();
+        else if (selected) play();
+        else play(track);
+    }
+
+    return (
+        <button {...{ onClick }}>
+            <Icon icon={playing() ? faPause : faPlay} />
+        </button>
+    );
+}
+
+export default usePlayer;
--- src/components/ActiveSong.tsx
+++ src/components/ActiveSong.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Cell from './Cell';
-import { IActiveTrack, IAlbum } from '../api/Models';
+import { IAlbum, ITrack } from '../api/Models';
 import { useApi } from '../api/Hooks';
 import { NO_COVER } from './App'
+import usePlayer from '../api/Audio';
 
-function ActiveSong({ track }: { track: IActiveTrack }) {
+function ActiveSong() {
+    const { track } = usePlayer();
+    return track ? <ActiveCover {...track} /> : null;
+}
+
+function ActiveCover(track: ITrack) {
     const [album] = useApi<IAlbum>(track.album);
 
     return (
